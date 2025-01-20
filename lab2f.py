560991df920dafafa50f5f76f249d802e10614a2
Dechen Phub <dphub@myseneca.ca>
2025-01-20 16:31:25 -0500

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Author: Dechen Phub
+# Author ID: 148596190
+# Date Created: 2025/01/20
+ 
+import sys 
+
+time = 10
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(str(timer))
+    timer = timer - 1
+    
+print('blast off!')
