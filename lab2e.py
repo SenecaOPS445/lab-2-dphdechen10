2fe5c84ea3ab48074d1d02b27ddd3b73ad68249b
Dechen Phub <dphub@myseneca.ca>
2025-01-20 15:36:44 -0500

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Author: Dechen Phub
+# Author ID: 148596190
+# Date Created: 2025/01/20
+ 
+timer = 10
+
+while timer != 0:
+    print(str(timer))
+    timer = timer - 1
+    
+print('blast off!')
+          
