c06fd1fd776d59a87bfad8ee3e319a06e737ed63
Dechen Phub <dphub@myseneca.ca>
2025-01-22 10:07:06 -0500

    Added lab2g

--- lab2g.py
+++ lab2g.py
@@ -5,15 +5,13 @@
  
 import sys
 
-
 if len(sys.argv) == 1:
     timer = 3
 else:
     timer = int(sys.argv[1])
 
-
 while timer > 0:
     print(timer)
     timer -= 1
 
-print("blast off!")
+print("blast off")
